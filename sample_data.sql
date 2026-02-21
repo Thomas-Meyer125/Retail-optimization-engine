f55e82bd85610b36751225afb981d9361b32ba7d
Thomas J. Meyer <tmeyer125@gmail.com>
2026-02-20 23:02:58 -0500

    Create sample_data.sql

---
+++ sample_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO products 
+(product_name, category, weekly_sales, profit_margin, shelf_width_inches)
+VALUES
+('Brand A Cereal', 'Cereal', 520, 0.32, 8),
+('Brand B Cereal', 'Cereal', 410, 0.28, 7),
+('Organic Granola', 'Cereal', 300, 0.45, 6),
+('Corn Flakes', 'Cereal', 650, 0.25, 9),
+('Family Oats', 'Cereal', 380, 0.30, 8),
+('Protein Bars', 'Snacks', 720, 0.40, 5),
+('Chips Classic', 'Snacks', 680, 0.22, 6),
+('Trail Mix', 'Snacks', 450, 0.38, 5),
+('Pretzels', 'Snacks', 390, 0.27, 5),
+('Fruit Snacks', 'Snacks', 610, 0.33, 6);
